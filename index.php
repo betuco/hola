5d3a84dec01e6cd602b5f7647229dfcfa9d4f802
bob <botudo.8.31mail.com>
2023-03-21 15:37:54 -0600

    mi comit

---
+++ index.php
@@ -0,0 +1,83 @@
+
+<html lang="es">
+    <head>
+        <meta charset="utf--8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shorcut icon" href="img/inteligencia-artificial.png">
+        <title>bienvenido</title><!--titulo de la página-->
+        <!--enlade cdn para bootstrap-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!--js cdn-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+        
+
+    </head>
+    <body >
+
+        <nav class="nav nav-pills nav-justified alert-primary" >
+            <a class="nav-link" href="#"><img src="img/inteligencia-artificial.png" style="width:2rem;"> &nbsp;INICIO</a>
+            <a class="nav-link" href="#"><div class="custom-control custom-switch">            
+                <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                    <label class="custom-control-label" for="customSwitch1">Toca para cambiar el dise&ntilde;o </label>
+                </div>    
+            </a>
+            <a class="nav-item nav-link" href="#"></a>
+            <a class="nav-item" href="#">
+                <audio controls preload="auto" autoplay="true" loop="true">
+                    <source src="lost.mp3" type="audio/mp3">
+                    Tu navegador no soporta audio HTML5.
+                </audio>
+            </a>
+        </nav>
+       
+       
+
+        <div class="container">
+            <div class="row">
+                <div class="col-4 border">bob</div>
+                <div class="col-4 border">bob</div>
+                <div class="col-4 border">bob
+                    <!--<div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>-->
+                    </div>   
+                </div>
+
+            <div class="row">
+                <div class="col-4 border">bob</div>
+                <div class="col-4 border">bob</div>
+                <div class="col-4 border">bob</div>   
+            </div>
+
+
+
+
+            <a href="es.qr-code-generator.com/a1/?gclid=Cj0KCQiA6rCgBhDVARIsAK1kGPJnEavnCmJV_896BX35k5Z4LT40ywoLdGTv2vIRGab6ZniGPDAzwkoaAplDEALw_wcB&campaignid=12188749584&adgroupid=117471706735&cpid=aab95194-21ff-470f-9d01-65584e439964&gclid=Cj0KCQiA6rCgBhDVARIsAK1kGPJnEavnCmJV_896BX35k5Z4LT40ywoLdGTv2vIRGab6ZniGPDAzwkoaAplDEALw_wcB" border="0" style="cursor:default" rel="nofollow"><img src="https://chart.googleapis.com/chart?cht=qr&chl=http%3A%2F%2Fmalandroterco%40live.com.mx&chs=180x180&choe=UTF-8&chld=L|2"></a>
+            </div>
+        </div> <!-- end container -->
+
+
+    </body>
+</html>
+<script>
+    var miCheckbox = document.getElementById('customSwitch1');
+    let cuerpo = document.getElementById("cuerpo");
+    miCheckbox.addEventListener('click', function() {
+        if(miCheckbox.checked) {
+           //document.body.innerHTML += "<button>bob</button>";
+          
+            
+        } else {
+            
+        }
+    });
+</script>    
